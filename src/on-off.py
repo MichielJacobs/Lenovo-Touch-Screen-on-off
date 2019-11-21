0893d0af1fb861dc43dcfe233e5c9427c8b1d0b4
MichielJacobs <33846471+MichielJacobs@users.noreply.github.com>
2019-11-21 17:34:10 +0100

    Delete on-off.py

--- src/on-off.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system('start devmanview-x64/DevManView.exe /enable "HID-compliant touch screen"')
-# os.system('start devmanview-x64/DevManView.exe /disable "HID-compliant touch screen"')
